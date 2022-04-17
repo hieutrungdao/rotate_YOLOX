8429c8b756e7f8fc52c095c87f3cbd967aa69056
hieutrungdao <hieutrungdao.cs@gmail.com>
2022-04-17 14:39:19 +0700

    update to train full dataset

--- script/train.sh
+++ script/train.sh
@@ -1,2 +1,2 @@
-python tools/train.py -f exps/p50.py -d 1 -b 1 --fp16 -o -c yolox_s.pth
+python tools/train.py -f exps/rotated_ds.py -d 1 -b 12 --fp16 -o -c yolox_s.pth
 # CUDA_LAUNCH_BLOCKING=1 python tools/train.py -f exps/p50.py -d 1 -b 1 --fp16 -o -c yolox_s.pth
