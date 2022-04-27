6b14ff10a980646c9bcc9e7bae285908127f61cf
hieutrungdao <hieutrungdao.cs@gmail.com>
2022-04-27 15:21:11 +0700

    update vis

--- script/train.sh
+++ script/train.sh
@@ -3,4 +3,4 @@
 # CUDA_LAUNCH_BLOCKING=1 python tools/train.py -f exps/nano.py -d 1 -b 6 --fp16 -o
 # CUDA_LAUNCH_BLOCKING=1 python tools/train.py -f exps/nano.py -d 1 -b 8 --fp16 -o --resume
 # CUDA_LAUNCH_BLOCKING=1 python tools/train.py -f exps/nano.py -d 1 -b 6 --fp16 -o -c pretrain.pth
-CUDA_LAUNCH_BLOCKING=1 python tools/train.py -f exps/pharma.py -d 1 -b 8 --fp16 -o -c epoch_196_ckpt.pth
+CUDA_LAUNCH_BLOCKING=1 python tools/train.py -f exps/p50.py -d 1 -b 1 --fp16 -o -c epoch_196_ckpt.pth
